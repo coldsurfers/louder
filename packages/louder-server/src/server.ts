ae89167b0bf16d9fcd0995aafb3800bd42150182
yungblud <killi8n@gmail.com>
2023-12-31 21:08:54 +0900

    feat: server run

--- packages/louder-server/src/server.ts
+++ packages/louder-server/src/server.ts
@@ -1,8 +1,20 @@
-import Fastify from 'fastify'
+import Fastify from "fastify";
 
 const fastify = Fastify({
-    ignoreTrailingSlash: true,
-    logger: {
-      level: 'info',
-    },
-  })
+  ignoreTrailingSlash: true,
+  logger: {
+    level: "info",
+  },
+});
+
+async function main() {
+  try {
+    await fastify.listen({ port: 8001, host: "0.0.0.0" });
+    fastify.log.info("server started", process.env.NODE_ENV);
+  } catch (e) {
+    fastify.log.error(e);
+    process.exit(1);
+  }
+}
+
+main();
