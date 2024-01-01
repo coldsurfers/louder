dd105a0d91a68487ee2651ca8830f823e47f59b6
yungblud <killi8n@gmail.com>
2024-01-01 16:30:25 +0900

    feat: implemented onRequest on admin hook

--- packages/louder-server/src/api/routes/admin.ts
+++ packages/louder-server/src/api/routes/admin.ts
@@ -5,6 +5,9 @@ import {
 } from "../controllers/admin.ctrl";
 
 const adminRoute: FastifyPluginCallback = (fastify, opts, done) => {
+  fastify.addHook("onRequest", async (req) => {
+    await req.jwtDecode();
+  });
   fastify.post("/admin/posts", postAdminPostCtrl);
   fastify.post("/admin/uploads", postAdminUploadTrack);
   done();
