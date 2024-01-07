30aef0443ebb0d8420e8f8f2930d51eee927d7e0
yungblud <killi8n@gmail.com>
2024-01-07 15:48:59 +0900

    feat: implemented fetch

--- packages/louder-client-next/src/app/page.tsx
+++ packages/louder-client-next/src/app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
+import { cache } from 'react'
+
+const fetchInitial = cache(
+  // eslint-disable-next-line no-return-await
+  async () => await fetch('http://0.0.0.0:8001/v1/post?page=1')
+)
+
+export default async function Home() {
+  const data = await fetchInitial()
+  console.log(await data.json())
   return null
 }
